b46e189010337dd79fc8c9e2ff1fcc4943cf0c95
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-17 23:38:41 +0300

    Update main.c

--- DivideEtEmpera/BinarySearch/main.c
+++ DivideEtEmpera/BinarySearch/main.c
@@ -1,6 +1,7 @@
+#include <assert.h>
 #include <stdio.h>
 
-int BinarySearch(int array[], int left, int right, int value) {
+int BinarySearch(const int array[], int left, int right, int value) {
 
     while (left <= right) {
         int middle = left + (right - left) / 2;
@@ -19,6 +20,15 @@ int BinarySearch(int array[], int left, int right, int value) {
 }
 
 int main() {
+
+    int array[] = {-65, -12, -3, 12, 13, 54, 89};
+    int length = sizeof(array) / sizeof(array[0]) - 1;
+
+    int valueTestOne = 13;
+    assert(BinarySearch(array, 0, length, valueTestOne) == 4);
+
+    int valueTestTwo = 15;
+    assert(BinarySearch(array, 0, length, valueTestTwo) == -1);
 
     return 0;
 }
