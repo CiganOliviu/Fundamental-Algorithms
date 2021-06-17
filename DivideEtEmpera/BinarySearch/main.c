068a48666e1cfc50a1c68902c69449c5babfad13
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-17 23:29:50 +0300

    Update main.c
    Implement BinarySearch function

--- DivideEtEmpera/BinarySearch/main.c
+++ DivideEtEmpera/BinarySearch/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int BinarySearch(int array[], int left, int right, int value) {
-    return 0;
+
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+
+        if (array[middle] == value)
+            return middle;
+
+        if (array[middle] < value)
+            left = middle + 1;
+
+        if (array[middle] > value)
+            right = middle - 1;
+    }
+
+    return -1;
 }
 
 int main() {
