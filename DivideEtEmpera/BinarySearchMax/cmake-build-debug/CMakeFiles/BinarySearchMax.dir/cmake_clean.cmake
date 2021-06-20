9315cbb2fc9fc4cc7a3a6ee513752a242880862c
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-20 14:05:24 +0300

    Create BinarySearchMax

---
+++ DivideEtEmpera/BinarySearchMax/cmake-build-debug/CMakeFiles/BinarySearchMax.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BinarySearchMax.exe"
+  "BinarySearchMax.exe.manifest"
+  "BinarySearchMax.pdb"
+  "CMakeFiles/BinarySearchMax.dir/main.c.obj"
+  "libBinarySearchMax.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BinarySearchMax.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
