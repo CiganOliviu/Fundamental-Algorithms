368c8439345539859ed0918925f0393fea8f9926
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-20 23:51:24 +0300

    Update BubbleSort

--- Sorting/BubbleSort/main.c
+++ Sorting/BubbleSort/main.c
@@ -3,7 +3,7 @@
 void OutputArray(int length, int * array) {
 
     for (int it = 0; it < length; ++it)
-        printf("%d", array[it]);
+        printf("%d ", array[it]);
 }
 
 void InterchangeVariables(int * var1, int * var2) {
@@ -17,11 +17,18 @@ void BubbleSort(int length, int * array) {
 
     for (int it = 0; it < length - 1; ++it)
         for (int jit = length - 1; jit > 1; jit--)
-            if (array[it] < array[jit - 1])
-                InterchangeVariables(array + it, array + jit - 1);
+            if (array[jit] < array[jit - 1])
+                InterchangeVariables(array + jit, array + jit - 1);
 }
 
 int main() {
-    
+
+    int array[] = {-65, -12, -3, 12, 13, 54, 89};
+    int length = sizeof(array) / sizeof(array[0]) - 1;
+
+    BubbleSort(length, array);
+
+    OutputArray(length, array);
+
     return 0;
 }
