c54f3b0f76e382cfed0d5777a376bf5b2b552bc3
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-20 14:10:37 +0300

    Create BinarySearchMin

---
+++ DivideEtEmpera/BinarySearchMin/cmake-build-debug/CMakeFiles/BinarySearchMin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BinarySearchMin.exe"
+  "BinarySearchMin.exe.manifest"
+  "BinarySearchMin.pdb"
+  "CMakeFiles/BinarySearchMin.dir/main.c.obj"
+  "libBinarySearchMin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BinarySearchMin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- DivideEtEmpera/BinarySearchMin/main.c
+++ DivideEtEmpera/BinarySearchMin/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int GetMaxViaBinarySearch(int * array, int startIndex, int finishIndex) {
+int GetMinViaBinarySearch(int * array, int startIndex, int finishIndex) {
 
     if (startIndex == finishIndex)
         return array[startIndex];
 
     int middle = (startIndex + finishIndex) / 2;
-    int termOne = GetMaxViaBinarySearch(array, startIndex, middle);
-    int termTwo = GetMaxViaBinarySearch(array, middle + 1, finishIndex);
+    int termOne = GetMinViaBinarySearch(array, startIndex, middle);
+    int termTwo = GetMinViaBinarySearch(array, middle + 1, finishIndex);
 
-    if (termOne > termTwo)
+    if (termOne < termTwo)
         return termOne;
 
     return termTwo;
@@ -20,7 +20,7 @@ int main() {
     int array[] = {-65, -12, -3, 12, 13, 54, 89};
     int length = sizeof(array) / sizeof(array[0]) - 1;
 
-    printf("%d", GetMaxViaBinarySearch(array, 0, length));
+    printf("%d", GetMinViaBinarySearch(array, 0, length));
 
     return 0;
 }
