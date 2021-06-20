89f74fff68e9a89920da3638e3a75f17625a9afe
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-20 23:54:18 +0300

    Create MinimumValueSort

---
+++ Sorting/MinimumValueSort/cmake-build-debug/CMakeFiles/MinimumValueSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinimumValueSort.dir/main.c.obj"
+  "MinimumValueSort.exe"
+  "MinimumValueSort.exe.manifest"
+  "MinimumValueSort.pdb"
+  "libMinimumValueSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MinimumValueSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sorting/MinimumValueSort/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void OutputArray(int length, int * array) {
+
+    for (int it = 0; it < length; ++it)
+        printf("%d ", array[it]);
+}
+
+void InterchangeVariables(int * var1, int * var2) {
+
+    *var1 = *var1 + *var2;
+    *var2 = *var1 - *var2;
+    *var1 = *var1 - *var2;
+}
+
+void MinimumValueSort(int length, int * array) {
+
+    for (int it = 0; it < length; it++)
+        for (int jit = it + 1; jit < length; ++jit) {
+            if (array[it] > array[jit])
+                InterchangeVariables(array + it, array + jit);
+        }
+}
+
+int main() {
+
+    int array[] = {-65, -12, -3, 12, 13, 54, 89};
+    int length = sizeof(array) / sizeof(array[0]) - 1;
+
+    MinimumValueSort(length, array);
+
+    OutputArray(length, array);
+
+    return 0;
+}
