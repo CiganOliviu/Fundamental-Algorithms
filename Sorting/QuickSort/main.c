608a337d20acd1dacecbb117fab8e3a641047c3a
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-21 16:22:41 +0300

    Update QuickSort

--- Sorting/QuickSort/main.c
+++ Sorting/QuickSort/main.c
@@ -9,8 +9,31 @@ void OutputArray(int length, int * array) {
         printf("%d ", array[it]);
 }
 
-void QuickSort(int length, int * array) {
+void QuickSort(int startIndex, int lastIndex, int * array) {
 
+    int aux, min, max, separator;
+    min = startIndex;
+    max = lastIndex;
+    separator = array[(startIndex + lastIndex) / 2];
+
+    do {
+        while (array[min] < separator)
+            min++;
+        while (array[max] > separator)
+            max--;
+
+        if (min <= max) {
+            aux = array[min];
+            array[min++] = array[max];
+            array[max--] = aux;
+        }
+    } while (min <= max);
+
+    if (startIndex < max)
+        QuickSort(startIndex, max, array);
+
+    if (min < lastIndex)
+        QuickSort(min, lastIndex, array);
 }
 
 int main() {
@@ -18,7 +41,7 @@ int main() {
     int array[] = {-65, -12, -3, 12, 13, 54, 89};
     int length = sizeof(array) / sizeof(array[0]) - 1;
 
-    QuickSort(length, array);
+    QuickSort(0, length, array);
 
     OutputArray(length, array);
 
